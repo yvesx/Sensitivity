950763ffaca1d36b2865c9a69e3299f68dc0e2fd
Yves Xie <yxi389@gmail.com>
2013-05-21 15:36:09 -0500

    getting beyond binary: ratings data

--- sensitivityRecSyS.py
+++ sensitivityRecSyS.py
@@ -13,15 +13,15 @@ import json
 from random import randrange
 import random
 
-db = MySQLdb.connect(host="xxxxx", port=3306, user="xxxxx", passwd="xxxx")
+db = MySQLdb.connect(host="10.1.1.xxxxx", port=3306, user="xxxxx", passwd="xxxxx")
 cursor = db.cursor()
 # kindle amazon amazon-related, 
-fids = [1,6,8364,838,8448,18,3397,32,17,2129,554,366,506,8163,55716,8164,395,8091,24,23,16]
+fids = [1,6,8364,838,7,18,3397,32,17,2129,554,366,506,8163,14,8164,395,8091,24,23,16,4,15,28,30,31,38,35]
 rows_to_write = {}
 for fid in fids:
     print "processing %s ..." % (fid)
     # create cache table
-    query = "SELECT user_id , fid FROM fb_fe.fb_comment_like_access WHERE fid=%s LIMIT 80000" % (fid)
+    query = "SELECT user_id , fid , num_of_likes+num_of_comments+num_of_liked FROM fb_fe.fb_comment_like_access WHERE fid=%s LIMIT 200000" % (fid)
     try:
         cursor.execute(query)
     except:
@@ -30,16 +30,21 @@ for fid in fids:
     for j in xrange(cursor.rowcount):
         row = cursor.fetchone()
         try:
-            rows_to_write[row[0]].append(row[1])
+            val = int(row[2])
         except:
-            rows_to_write[row[0]] = [row[1]]
+            val = 1
+        try:
+            rows_to_write[row[0]][row[1]] = val
+        except:
+            rows_to_write[row[0]] = dict()
+            rows_to_write[row[0]][row[1]] = val
 
-with open("sensitivityRecSyS.dat", "a") as myfile:
+with open("large.dat", "a") as myfile:
     for key in rows_to_write.keys():
         access_list = rows_to_write[key]
-        mask = [ 1 if fid in access_list else 0 for fid in fids ]
+        mask = [ access_list[fid] if fid in access_list else 0 for fid in fids ]
         # only keeyp nonsparse ones.
-        if sum(mask) > 1:
+        if sum([1 for x in mask if x > 0]) > 1:
             mask = [ str(item) for item in mask ]
             myfile.write(' '.join(mask))
             myfile.write('\n')
